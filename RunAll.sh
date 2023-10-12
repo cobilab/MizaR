f9d3728af3e1a8696752662add379a448d2253d3
pratas <6882743+pratas@users.noreply.github.com>
2023-10-12 12:13:42 +0100

    extra added

--- RunAll.sh
+++ RunAll.sh
@@ -21,10 +21,11 @@ for(( x = 100 ; x <= 1000 ; x += 100 ));
   B_BASES_SORTED=`cat report_sorted.txt | grep Bases | awk '{ print $4; }'`;
   B_QUALS_SORTED=`cat report_sorted.txt | grep Quals | awk '{ print $4; }'`;
   B_PERMUTATION=`cat report_permutations.txt | awk '{ print $1; }'`; 
-  B_TOTAL_ORIGINAL=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
-  B_TOTAL_SORTED=`ls -la merged_reads.fq.fqz | awk '{ print $5; }'`;
+  B_TOTAL_ORIGINAL=`ls -la merged_reads-ORIGINAL.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED + $B_PERMUTATION" | bc -l`;
   #
-  printf "$x\t$B_NAMES_ORIGINAL\t$B_BASES_ORIGINAL\t$B_QUALS_ORIGINAL\t$B_NAMES_SORTED\t$B_BASES_SORTED\t$B_QUALS_SORTED\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\n" >> sequences.txt
+  printf "$x\t$B_NAMES_ORIGINAL\t$B_BASES_ORIGINAL\t$B_QUALS_ORIGINAL\t$B_NAMES_SORTED\t$B_BASES_SORTED\t$B_QUALS_SORTED\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\t$B_TOTAL_SORTED_PERMUTATION\n" >> sequences.txt
   #
   done
 #
@@ -47,10 +48,11 @@ for(( x = 1 ; x <= 100 ; x += 10 ));
   B_BASES_SORTED=`cat report_sorted.txt | grep Bases | awk '{ print $4; }'`;
   B_QUALS_SORTED=`cat report_sorted.txt | grep Quals | awk '{ print $4; }'`;
   B_PERMUTATION=`cat report_permutations.txt | awk '{ print $1; }'`;
-  B_TOTAL_ORIGINAL=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
-  B_TOTAL_SORTED=`ls -la merged_reads.fq.fqz | awk '{ print $5; }'`;
+  B_TOTAL_ORIGINAL=`ls -la merged_reads-ORIGINAL.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED + $B_PERMUTATION" | bc -l`;
   #
-  printf "$x\t$B_NAMES_ORIGINAL\t$B_BASES_ORIGINAL\t$B_QUALS_ORIGINAL\t$B_NAMES_SORTED\t$B_BASES_SORTED\t$B_QUALS_SORTED\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\n" >> coverage.txt
+  printf "$x\t$B_NAMES_ORIGINAL\t$B_BASES_ORIGINAL\t$B_QUALS_ORIGINAL\t$B_NAMES_SORTED\t$B_BASES_SORTED\t$B_QUALS_SORTED\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\t$B_TOTAL_SORTED_PERMUTATION\n" >> coverage.txt
   #
   done
 #
