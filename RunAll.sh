43e3c753f147e122bed0e9708a3a2fb33a2d205b
pratas <6882743+pratas@users.noreply.github.com>
2023-10-20 14:42:39 +0100

    add support for other compressors

--- RunAll.sh
+++ RunAll.sh
@@ -15,18 +15,28 @@ for(( x = 100 ; x <= 1000 ; x += 100 ));
   --database $DB --cache $CACHE \
   --similarity 50 --output compressed.mr
   #
-  B_NAMES_ORIGINAL=`cat report_original.txt | grep Names | awk '{ print $4; }'`;
-  B_BASES_ORIGINAL=`cat report_original.txt | grep Bases | awk '{ print $4; }'`;
-  B_QUALS_ORIGINAL=`cat report_original.txt | grep Quals | awk '{ print $4; }'`;
-  B_NAMES_SORTED=`cat report_sorted.txt | grep Names | awk '{ print $4; }'`;
-  B_BASES_SORTED=`cat report_sorted.txt | grep Bases | awk '{ print $4; }'`;
-  B_QUALS_SORTED=`cat report_sorted.txt | grep Quals | awk '{ print $4; }'`;
   B_PERMUTATION=`cat report_permutations.txt | awk '{ print $1; }'`; 
-  B_TOTAL_ORIGINAL=`ls -la merged_reads.fq-ORIGINAL.fqz | awk '{ print $5; }'`;
-  B_TOTAL_SORTED=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
-  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED + $B_PERMUTATION" | bc -l`;
   #
-  printf "$x\t$B_NAMES_ORIGINAL\t$B_BASES_ORIGINAL\t$B_QUALS_ORIGINAL\t$B_NAMES_SORTED\t$B_BASES_SORTED\t$B_QUALS_SORTED\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\t$B_TOTAL_SORTED_PERMUTATION\n" >> sequences.txt
+  B_NAMES_ORIGINAL_FQZ=`cat report_original_fqz.txt | grep Names | awk '{ print $4; }'`;
+  B_BASES_ORIGINAL_FQZ=`cat report_original_fqz.txt | grep Bases | awk '{ print $4; }'`;
+  B_QUALS_ORIGINAL_FQZ=`cat report_original_fqz.txt | grep Quals | awk '{ print $4; }'`;
+  B_NAMES_SORTED_FQZ=`cat report_sorted_fqz.txt | grep Names | awk '{ print $4; }'`;
+  B_BASES_SORTED_FQZ=`cat report_sorted_fqz.txt | grep Bases | awk '{ print $4; }'`;
+  B_QUALS_SORTED_FQZ=`cat report_sorted_fqz.txt | grep Quals | awk '{ print $4; }'`;
+  #
+  B_TOTAL_ORIGINAL_LZMA=`ls -la merged_reads.fq-ORIGINAL.xz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_LZMA=`ls -la compressed.mr.xz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED_LZMA + $B_PERMUTATION" | bc -l`;  
+  #
+  B_TOTAL_ORIGINAL_JARVIS=`ls -la merged_reads.fq-ORIGINAL.tar | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_JARVIS=`ls -la compressed.mr.tar | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED_JARVIS + $B_PERMUTATION" | bc -l`;
+  #
+  B_TOTAL_ORIGINAL_FQZ=`ls -la merged_reads.fq-ORIGINAL.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_FQZ=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED_FQZ + $B_PERMUTATION" | bc -l`;
+  #
+  printf "$x\t$B_NAMES_ORIGINAL_FQZ\t$B_BASES_ORIGINAL_FQZ\t$B_QUALS_ORIGINAL_FQZ\t$B_NAMES_SORTED_FQZ\t$B_BASES_SORTED_FQZ\t$B_QUALS_SORTED_FQZ\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\t$B_TOTAL_SORTED_PERMUTATION\t$B_TOTAL_ORIGINAL_LZMA\t$B_TOTAL_SORTED_JARVIS\t$B_TOTAL_SORTED_PERMUTATION\t$B_TOTAL_ORIGINAL_FQZ\t$B_TOTAL_SORTED_FQZ\t$B_TOTAL_SORTED_PERMUTATION\n" >> sequences.txt
   #
   done
 #
@@ -42,18 +52,28 @@ for(( x = 1 ; x <= 100 ; x += 10 ));
   --database $DB --cache $CACHE \
   --similarity 50 --output compressed.mr
   #
-  B_NAMES_ORIGINAL=`cat report_original.txt | grep Names | awk '{ print $4; }'`;
-  B_BASES_ORIGINAL=`cat report_original.txt | grep Bases | awk '{ print $4; }'`;
-  B_QUALS_ORIGINAL=`cat report_original.txt | grep Quals | awk '{ print $4; }'`;
-  B_NAMES_SORTED=`cat report_sorted.txt | grep Names | awk '{ print $4; }'`;
-  B_BASES_SORTED=`cat report_sorted.txt | grep Bases | awk '{ print $4; }'`;
-  B_QUALS_SORTED=`cat report_sorted.txt | grep Quals | awk '{ print $4; }'`;
   B_PERMUTATION=`cat report_permutations.txt | awk '{ print $1; }'`;
-  B_TOTAL_ORIGINAL=`ls -la merged_reads.fq-ORIGINAL.fqz | awk '{ print $5; }'`;
-  B_TOTAL_SORTED=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
-  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED + $B_PERMUTATION" | bc -l`;
   #
-  printf "$x\t$B_NAMES_ORIGINAL\t$B_BASES_ORIGINAL\t$B_QUALS_ORIGINAL\t$B_NAMES_SORTED\t$B_BASES_SORTED\t$B_QUALS_SORTED\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\t$B_TOTAL_SORTED_PERMUTATION\n" >> coverage.txt
+  B_NAMES_ORIGINAL_FQZ=`cat report_original_fqz.txt | grep Names | awk '{ print $4; }'`;
+  B_BASES_ORIGINAL_FQZ=`cat report_original_fqz.txt | grep Bases | awk '{ print $4; }'`;
+  B_QUALS_ORIGINAL_FQZ=`cat report_original_fqz.txt | grep Quals | awk '{ print $4; }'`;
+  B_NAMES_SORTED_FQZ=`cat report_sorted_fqz.txt | grep Names | awk '{ print $4; }'`;
+  B_BASES_SORTED_FQZ=`cat report_sorted_fqz.txt | grep Bases | awk '{ print $4; }'`;
+  B_QUALS_SORTED_FQZ=`cat report_sorted_fqz.txt | grep Quals | awk '{ print $4; }'`;
+  #
+  B_TOTAL_ORIGINAL_LZMA=`ls -la merged_reads.fq-ORIGINAL.xz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_LZMA=`ls -la compressed.mr.xz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED_LZMA + $B_PERMUTATION" | bc -l`;
+  #
+  B_TOTAL_ORIGINAL_JARVIS=`ls -la merged_reads.fq-ORIGINAL.tar | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_JARVIS=`ls -la compressed.mr.tar | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED_JARVIS + $B_PERMUTATION" | bc -l`;
+  #
+  B_TOTAL_ORIGINAL_FQZ=`ls -la merged_reads.fq-ORIGINAL.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_FQZ=`ls -la compressed.mr.fqz | awk '{ print $5; }'`;
+  B_TOTAL_SORTED_PERMUTATION=`echo "$B_TOTAL_SORTED_FQZ + $B_PERMUTATION" | bc -l`;
+  #
+  printf "$x\t$B_NAMES_ORIGINAL_FQZ\t$B_BASES_ORIGINAL_FQZ\t$B_QUALS_ORIGINAL_FQZ\t$B_NAMES_SORTED_FQZ\t$B_BASES_SORTED_FQZ\t$B_QUALS_SORTED_FQZ\t$B_PERMUTATION\t$B_TOTAL_ORIGINAL\t$B_TOTAL_SORTED\t$B_TOTAL_SORTED_PERMUTATION\t$B_TOTAL_ORIGINAL_LZMA\t$B_TOTAL_SORTED_JARVIS\t$B_TOTAL_SORTED_PERMUTATION\t$B_TOTAL_ORIGINAL_FQZ\t$B_TOTAL_SORTED_FQZ\t$B_TOTAL_SORTED_PERMUTATION\n" >> coverage.txt
   #
   done
 #
